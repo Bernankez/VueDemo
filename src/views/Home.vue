bcf46c7b0841e832e3783e36475d032831efc85a
cole <bernankez@qq.com>
2022-04-24 22:14:36 +0800

    feat: 添加导航

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,7 @@
-<template></template>
+<template>
+  <router-link to="/switcher">Switcher</router-link><br>
+  <router-link to="/cyber-button">CyberButton</router-link>
+</template>
 
 <script setup lang="ts"></script>
 
